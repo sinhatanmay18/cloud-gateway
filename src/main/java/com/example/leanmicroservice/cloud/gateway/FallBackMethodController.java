94750a70c0d06fd16bbf5c07a258dfb0d6a30782
Tanmay Sinha <sinha.t18@outlook.com>
2023-12-12 11:05:36 +0530

    first commit for cloud gateway.

---
+++ src/main/java/com/example/leanmicroservice/cloud/gateway/FallBackMethodController.java
@@ -0,0 +1,17 @@
+package com.example.leanmicroservice.cloud.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class FallBackMethodController {
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod(){
+        return "User Service is taking longer than expected, try again later";
+    }
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBack(){
+        return "Department Service is taking longer than expected, try again later";
+    }
+}
